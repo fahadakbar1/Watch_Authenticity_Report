70cd740a1abdc80b57c86ab2f84707a3c1c00ef0
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-10-07 15:12:40 +0500

    Configured TailwindCSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1>Authenticitry Report Generator</h1>
+       <h1 className="text-3xl font-bold underline">Authenticitry Report Generator</h1>
     </>
   );
 };
